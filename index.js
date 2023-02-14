0d9da7e114e62c4f292ba19208f71fa98c15c59d
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-02-14 17:40:03 +0300

    17/01/23

---
+++ index.js
@@ -0,0 +1,45 @@
+console.log("Hello world");
+// console.log("Hello world");
+// console.log("Jyldyz");
+
+let example = "Ручка";
+let example2 = "Январь";
+
+console.log(example);
+//console.log(example2);
+
+example = "pencil";
+
+//console.log(example);
+
+let myCity ="Karakol";
+let myFirstName = "Jyldyz";//строка
+let myFavouriteNumber = 8.2;// число
+
+console.log("My city is " + myCity);
+console.log("My name is " + myFirstName);
+console.log("My favourite number is " + myFavouriteNumber);
+
+//Чему равняется площадь (area)прямоугольника с шириной 30 (а) и длиной 50 (b) см.
+let a = 30;
+let b = 50;
+let area = a*b;
+console.log("Площадь равняется " + area);
+
+//Клиент хочет обменять 10000 (kgs)сом на доллары.
+//Сколько он получить долларов (usd) по курсу 87.5 (rate)
+let kgs = 10000;
+let rate = 87.5;
+let usd = kgs / rate;
+console.log("Клиент получит "+ usd);
+
+//Клиент хочет отправить посылку размером
+//высота 30 см, ширина 50 см, длина 40 см
+//Чему равняется объем посылки.
+let wieght = 30;
+let height = 50;
+let length = 40;
+let volume = wieght * height * length
+console.log("обЪем посылки равняется " + volume )
+
+
